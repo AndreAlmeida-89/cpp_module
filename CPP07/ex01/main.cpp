64bf263d1576a87dee7b4cdf90d327a4318f3f9e
André Almeida <andre.felipe@xpi.com.br>
2024-05-15 22:20:00 -0300

    finish ex01

--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -5,45 +5,48 @@ int main(void)
 	{
 		typedef int type;
 		type arr[] = {1, 2, 3, 4, 5};
-		size_t size = sizeof(arr)/sizeof(type);
-		iter(arr, size, printContent);
-		iter(arr, size, doubleValue);
+		size_t size = sizeof(arr) / sizeof(type);
+		::iter(arr, size, printContent);
+		::iter(arr, size, doubleValue);
 		std::cout << std::endl;
-		iter(arr, size, printContent);
-		std::cout << std::endl << std::endl;
+		::iter(arr, size, printContent);
+		std::cout << std::endl
+				  << std::endl;
 	}
 
 	{
 		typedef double type;
 		type arr[] = {1.0, 2.0, 3.0, 4.0, 5.0};
-		size_t size = sizeof(arr)/sizeof(type);
-		iter(arr, size, printContent);
-		iter(arr, size, doubleValue);
+		size_t size = sizeof(arr) / sizeof(type);
+		::iter(arr, size, printContent);
+		::iter(arr, size, doubleValue);
 		std::cout << std::endl;
-		iter(arr, size, printContent);
-		std::cout << std::endl << std::endl;
+		::iter(arr, size, printContent);
+		std::cout << std::endl
+				  << std::endl;
 	}
 
 	{
 		typedef float type;
 		type arr[] = {1.0, 2.0, 3.0, 4.0, 5.0};
-		size_t size = sizeof(arr)/sizeof(type);
-		iter(arr, size, printContent);
-		iter(arr, size, doubleValue);
+		size_t size = sizeof(arr) / sizeof(type);
+		::iter(arr, size, printContent);
+		::iter(arr, size, doubleValue);
 		std::cout << std::endl;
-		iter(arr, size, printContent);
-		std::cout << std::endl << std::endl;
+		::iter(arr, size, printContent);
+		std::cout << std::endl
+				  << std::endl;
 	}
 
 	{
 		typedef std::string type;
 		type arr[] = {"1", "2", "3", "4", "5"};
-		size_t size = sizeof(arr)/sizeof(type);
-		iter(arr, size, printContent);
-		iter(arr, size, doubleValue);
+		size_t size = sizeof(arr) / sizeof(type);
+		::iter(arr, size, printContent);
+		::iter(arr, size, doubleValue);
 		std::cout << std::endl;
-		iter(arr, size, printContent);
-		std::cout << std::endl << std::endl;
+		::iter(arr, size, printContent);
+		std::cout << std::endl;
 	}
 	return (0);
 }
